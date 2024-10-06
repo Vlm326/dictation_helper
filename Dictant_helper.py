5f6edc3a94cdce97ce2e5cccb6a554ca818701a8
Vlm326 <morozvv75@gmail.com>
2024-10-06 14:00:36 +0300

    some fixes

--- Dictant_helper.py
+++ Dictant_helper.py
@@ -1,6 +1,5 @@
 import pyttsx3
-from PySimpleGUI import popup_get_file
-import PySimpleGUI as sg
+from PySimpleGUI import popup_get_file, popup
 from random import randint
 from time import time
 
@@ -10,9 +9,9 @@ def delete_bad_signes_from_words(word: str) -> str:
     return word
 
 while True:
-    path = sg.popup_get_file('Введите путь к файлу:', no_window=True)
+    path = popup_get_file('Введите путь к файлу:', no_window=True)
     if not path:
-        sg.popup('Файл не найден, попробуйте снова')
+        popup('Файл не найден, попробуйте снова')
         continue
     try:
         with open(path, encoding="utf-8") as f:
@@ -22,29 +21,29 @@ while True:
                     continue
                 if len(line.split()) > 1:
                     for word in line.split():
-                        content.append(delete_bad_signes_from_words(word.strip().lower()))
-
+                        if len(word) > 1 and word != ' nh':
+                            content.append(delete_bad_signes_from_words(word.strip().lower()))
                 else:
                     content.append(delete_bad_signes_from_words(line.strip().lower()))
         break
     except FileNotFoundError:
-        sg.popup('Файл не найден, попробуйте снова')
+        popup('Файл не найден, попробуйте снова')
     except Exception as e:
         print(e)
-        sg.popup(f"Произошла ошибка: {e}")
+        popup(f"Произошла ошибка: {e}")
 
 words = content
 errors = []
 engine = pyttsx3.init()
 start = time()
 seen_words = []
-for i in range(len(words)):
-    end = start - time()
+for j in range(len(words)):
+    end = abs(start - time())
     i = randint(0, len(words) - 1)
     while i in seen_words:
         i = randint(0, len(words) - 1)
     seen_words.append(i)
-    left = len(words) - i
+    left = len(words) - j
     engine.say(words[i])
     engine.runAndWait()
     print(f'Words: {len(words)}, words left: {left}, time: {end}')
